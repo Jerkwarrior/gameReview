d8a64181bca472e8a0c4d2f748d8c79a0b3e9918
Luke Marco <luke.am89@hotmail.co.uk>
2017-11-06 11:58:51 +0200

    Added jobs for image and video

---
+++ app/jobs/get_video_job.rb
@@ -0,0 +1,11 @@
+class GetVideoJob < ApplicationJob
+
+  def perform(igdb_id)
+    return if Game.where(id: igdb_id).exists?
+    ig = Igdb::Game.find(igdb_id)
+    ig.videos.each do |video|
+      video = Video.new(game_id: igdb_id, name: video.name, youtube_slug: video.video_id)
+      video.save!
+    end
+  end
+end
