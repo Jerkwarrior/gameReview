a4b5744be402075243d8c10e443c0689e24162ee
Luke Marco <luke.am89@hotmail.co.uk>
2017-11-12 15:53:07 +0200

    Changed migration Player to player

--- db/migrate/20171102173713_add_associations.rb
+++ db/migrate/20171102173713_add_associations.rb
@@ -4,7 +4,7 @@ class AddAssociations < ActiveRecord::Migration[5.1]
     add_reference :games, :game_mode, foreign_key: true
     add_reference :games, :genre, foreign_key: true
     add_reference :games, :keyword, foreign_key: true
-    add_reference :games, :Player_perspective, foreign_key: true
+    add_reference :games, :player_perspective, foreign_key: true
     add_reference :games, :theme, foreign_key: true
 
     add_reference :games, :image, foreign_key: true
@@ -14,7 +14,7 @@ class AddAssociations < ActiveRecord::Migration[5.1]
     add_reference :game_modes, :game, foreign_key: true
     add_reference :genres, :game, foreign_key: true
     add_reference :keywords, :game, foreign_key: true
-    add_reference :Player_perspectives, :game, foreign_key: true
+    add_reference :player_perspectives, :game, foreign_key: true
     add_reference :themes, :game, foreign_key: true
 
 
