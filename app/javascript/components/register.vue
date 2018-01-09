c9edb60d076ecfa7d8ae12782d0c8d0b213c4c19
Luke Marco <luke.am89@hotmail.co.uk>
2018-01-09 17:13:12 +0000

    Created basic user registration form

--- app/javascript/components/register.vue
+++ app/javascript/components/register.vue
@@ -1,6 +1,16 @@
 <template lang="html">
   <div class="register">
+    <!-- username, email, password -->
     <h1>REGISTER</h1>
+    <form>
+      Username: <br>
+      <input type="text" v-model="username">{{username}} <br>
+      Email Address: <br  >
+      <input type="email" v-model="email">{{email}} <br>
+      Password: <br>
+      <input type="password" v-model="password">{{password}} <br>
+      <input type="submit" name="" value="Submit">
+    </form>
   </div>
 </template>
 
