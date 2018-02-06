089b307b1d37942892c2b192244dcf5a84ed2707
Luke Marco <luke.am89@hotmail.co.uk>
2018-02-06 20:50:16 +0000

    Edited routes to use get request to logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users, skip: [:sessions]
   as :user do
-    get 'signin', to: 'devise/sessions#new', as: :new_user_session
-    post 'signin', to: 'devise/sessions#create', as: :user_session
-    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'users/sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post 'users/sign_in', to: 'devise/sessions#create', as: :user_session
+    get 'users/signout', to: 'devise/sessions#destroy',
+                         as: :destroy_user_session
   end
 
   resources :companies, only: [:index]
-  resources :games, only: [:index, :show], defaults: { format: 'json' } do
+  resources :games, only: %i[index show], defaults: { format: 'json' } do
     resources :reviews
   end
   root 'visitor#index'
