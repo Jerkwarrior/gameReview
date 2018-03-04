e1cbd16b3d9358329c16a5017f0d1274492dd8d2
Luke Marco <luke.am89@hotmail.co.uk>
2018-03-04 19:46:44 +0000

    Created user sign up with json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@
 class ApplicationController < ActionController::Base # :nodoc:
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
