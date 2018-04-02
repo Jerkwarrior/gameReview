32d123bfd43761dba97e2cdadd77c82785064c93
Luke Marco <luke.am89@hotmail.co.uk>
2018-04-02 12:51:07 +0100

    Changed companies to pull less data

--- app/jobs/get_test_data_job.rb
+++ app/jobs/get_test_data_job.rb
@@ -2,7 +2,7 @@ class GetTestDataJob < ApplicationJob # :nodoc:
   queue_as :default
 
   def perform
-    (1..5).each do |igdb_company_id|
+    (5..10).each do |igdb_company_id|
       GetCompanyJob.perform_later(igdb_company_id)
     end
   end
